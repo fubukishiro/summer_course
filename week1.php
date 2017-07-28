b5579768628b4d0bd1e3d515df028f1e894e5213
PhantomKang <30312981+PhantomKang@users.noreply.github.com>
2017-07-28 18:07:46 +0800

    Add files via upload

---
+++ week1.php
@@ -0,0 +1,115 @@
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <link rel="stylesheet" href="https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://cdn.bootcss.com/jquery/2.1.1/jquery.min.js"></script>
+    <script src="https://cdn.bootcss.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  </head>
+  <body style="background-color:rgb(233,233,233)">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-xs-5" style="background-color:rgb(255,255,255);margin:3% 4% 3% 4%;text-align:center">
+          <p style="font-size:25px;margin:5px 0px 5px">
+            Title here
+          </p>
+          <img src="hj.jpg" style="max-width:100%">
+          <p style="font-size:25px;margin:5px">
+            For more detail, please click the right column
+          </p>
+        </div>
+        <div class="col-xs-5" style="margin:3% 4% 3% 4%">
+          <div class="row">
+            <div class="btn-group col-sm-4 col-xs-6" style="margin:10px 0px 10px">
+              <center>
+                <button type="button" class="btn btn-default btn-lg dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="width:50%;background-color:rgb(239,173,79)">
+                  1
+                  <span class="caret">
+                  </span>
+                </button>
+                <ul class="dropdown-menu">
+                  <li><a>1A</a></li>
+                  <li><a>1B</a></li>
+                </ul>
+              </center>
+            </div>
+            <div class="btn-group col-sm-4 col-xs-6" style="margin:10px 0px 10px">
+              <center>
+                <button type="button" class="btn btn-default btn-lg" style="width:50%;background-color:rgb(239,173,79)">
+                  2
+                </button>
+              </center>
+            </div>
+            <div class="btn-group col-sm-4 col-xs-6" style="margin:10px 0px 10px">
+              <center>
+                <button type="button" class="btn btn-default btn-lg dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="width:50%;background-color:rgb(239,173,79)">
+                  3
+                  <span class="caret">
+                  </span>
+                </button>
+                <ul class="dropdown-menu">
+                  <li><a>3A</a></li>
+                  <li><a>3B</a></li>
+                </ul>
+              </center>
+            </div>
+            <div class="btn-group col-sm-4 col-xs-6" style="margin:10px 0px 10px">
+              <center>
+                <button type="button" class="btn btn-default btn-lg dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="width:50%;background-color:rgb(239,173,79)">
+                  4
+                  <span class="caret">
+                  </span>
+                </button>
+                <ul class="dropdown-menu">
+                  <li><a>4A</a></li>
+                  <li><a>4B</a></li>
+                </ul>
+              </center>
+            </div>
+            <div class="btn-group col-sm-4 col-xs-6" style="margin:10px 0px 10px">
+              <center>
+                <button type="button" class="btn btn-default btn-lg" style="width:50%;background-color:rgb(239,173,79)">
+                  5
+                </button>
+              </center>
+            </div>
+            <div class="btn-group col-sm-4 col-xs-6" style="margin:10px 0px 10px">
+              <center>
+                <button type="button" class="btn btn-default btn-lg" style="width:50%;background-color:rgb(239,173,79)">
+                  6
+                </button>
+              </center>
+            </div>
+            <div class="btn-group col-sm-4 col-xs-6" style="margin:10px 0px 10px">
+              <center>
+                <button type="button" class="btn btn-default btn-lg dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="width:50%;background-color:rgb(239,173,79)">
+                  7
+                  <span class="caret">
+                  </span>
+                </button>
+                <ul class="dropdown-menu">
+                  <li><a>7A</a></li>
+                  <li><a>7B</a></li>
+                </ul>
+              </center>
+            </div>
+            <div class="btn-group col-sm-4 col-xs-6" style="margin:10px 0px 10px">
+              <center>
+                <button type="button" class="btn btn-default btn-lg" style="width:50%;background-color:rgb(239,173,79)">
+                  8
+                </button>
+              </center>
+            </div>
+            <div class="btn-group col-sm-4 col-xs-6" style="margin:10px 0px 10px">
+              <center>
+                <button type="button" class="btn btn-default btn-lg" style="width:50%;background-color:rgb(239,173,79)">
+                  9
+                </button>
+              </center>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
